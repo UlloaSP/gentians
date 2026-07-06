a5e4aaf057e66676785b21e8d9cc75ea5930e398
Pablo Ulloa Santín <pablo.ulloa.santin@udc.es>
2026-07-06 09:17:48 +0200

    refactor: simplify parameter type hinting and streamline loop for coverage clause generation

--- gentians/asp/coverage.py
+++ gentians/asp/coverage.py
@@ -26,7 +26,7 @@ def _mask(values: "list[int]") -> int:
 
 
 def generate_clauses_for_coverage_interpretations(
-    interpretations: "list[Example]", positive: bool
+    interpretations: list[Example], positive: bool
 ) -> str:
     """
     Generates the clauses for the ASP solver to check the coverage.
@@ -35,24 +35,15 @@ def generate_clauses_for_coverage_interpretations(
     """
     parts: list[str] = []
     suffix: str = "cp" if positive else "cn"
-    cl_index = 0
-    for example in interpretations:
-        # inclusion
+    for cl_index, example in enumerate(interpretations):
         if len(example.included) > 0:
             parts.append(f"{suffix}i({cl_index}):- {example.included}.")
 
-            # if len(example.excluded) > 1:
-            # exclusion
         if len(example.excluded) > 0:
             for atom in split_top_level_args(example.excluded):
                 parts.append(f"{suffix}e({cl_index}):- {atom}.")
 
-            # if len(example.) > 2:
-            # context dependent examples
         if len(example.context) > 0:
-            # for atom in atoms[2].split(' '):
             parts.append(example.context + ".")
-
-        cl_index += 1
 
     return "\n".join(parts) + "\n\n"
